2f57da4938936ac7b2ac627e14571a43d3162585
Kunal Mishra <kunmishra2599@gmail.com>
2017-10-03 00:46:14 +0800

    added lost_send_nodes file

---
+++ lost_send_nodes/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="jumbotron">
+      <h1>My Daily Journal</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+.jumbotron{
+  text-align: center;
+  background-color: white;
+
+}
+.jumbotron h1{
+  font-family: 'Lato';
+  font-weight: 300;
+}
+
+
+</style>
